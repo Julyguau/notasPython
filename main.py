66592a49c50a48b238513ec9f31f88642c4e6c51
Julyguau <118016005+Julyguau@users.noreply.github.com>
2023-02-09 12:35:26 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,68 +1,22 @@
-"""
-
-desarrollar programa q solicite su nombre, apellido paterno y materno
-
-de ahì, recorrer cada cadena de texto y capitalizarla (se imprima en mayusculas) despues concatenar
-todas las cadenas a una nueva variable y usando slicing vamos a imprimir la primera mitad de la nueva cadena
-y en una nueva linea imprimir la otra mitad al revès
-preguntar al usuario cuàntas veces quiere que se imprima su nomber en pantalla y mostrarlo
-
-"""
 
 
-def capitalizar(x):
-    return x.capitalize()
+def valorAscii(cadena):
+    cadenaLista=list(cadena)
+    for i in range(0,len(cadenaLista)):
+        # esta línea obtiene el valor dentro de la tabla ascii del caracter   print(ord(i))
+       # if ord(i)>=65 and ord(i)<=90:   #mayúsculas rango  esta linea se simplifico a còmo esta abajo
+        if 65 <= ord(cadenaLista[i]) <= 90:
+            nuevoCaracter=ord(cadenaLista[i])+32
+            cadenaLista[i]=chr(nuevoCaracter)
+        # sin simplificar elif ord(i)>=97 and ord(i)<=122
+        elif 97 <= ord(cadenaLista[i]) <= 122:
+            nuevoCaracter=ord(cadenaLista[i])-32
+            cadenaLista[i]=chr(nuevoCaracter)
+    cadena=''.join(cadenaLista)
+    print(cadena)
 
 
-def concatenar(x,y,z):
-    return x + " " + y + " " + z
+frase=input("Ingresa tu frase:")
 
 
-def mitades(x):
-    tam=int(len(x)/2)
-    nuevaMitad=x[tam:]
-    print("\nla primera mitad del nombre es: "+x[:tam])
-    print("\nla segunda mitad al revés es: "+nuevaMitad[::-1])
-
-
-def imprimir(x,y):
-    i=0
-    while i < int (x):
-        print(y)
-
-        i += 1
-
-
-print("Ingresa tu nombre: ")
-nombre = input()
-
-apellidoMaterno=input("\nIngresa tu apellido materno:")
-
-apellidoPaterno=input("\nIngresa tu apellido paterno:")
-
-# ahora recorremos toda la cadena y la capitalizamos
-
-nombre=capitalizar(nombre)
-apellidoPaterno=capitalizar(apellidoPaterno)
-apellidoMaterno=capitalizar(apellidoMaterno)
-
-print(nombre)
-print(apellidoMaterno)
-print(apellidoPaterno)
-
-
-# ahora mostramos còmo se concatena cada cadena
-
-nombreCompleto=concatenar(nombre,apellidoPaterno,apellidoMaterno)
-
-print(nombreCompleto)
-
-mitades(nombreCompleto)
-
-#ahora veremos cuantas veces quiere el usuario imprimir su nombre
-
-veces=input("\n¿Cuántas veces deseas imprimir tu nombre?")
-
-imprimir(veces,nombreCompleto)
-
-
+valorAscii(frase)
